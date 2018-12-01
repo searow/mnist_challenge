0b63c5913a86aef3281b773b10108a4cf5fa15f4
David Rangel <drangelalarcon@gmail.com>
2018-12-01 15:08:16 -0800

    print Accuracy in main

--- run_attack.py
+++ run_attack.py
@@ -91,4 +91,4 @@ if __name__ == '__main__':
                                                               np.amin(x_adv),
                                                               np.amax(x_adv)))
   else:
-    run_attack(checkpoint, x_adv, config['epsilon'])
+    print("Accuracy = {}".format(run_attack(checkpoint, x_adv, config['epsilon'])))
