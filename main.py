403e8af47971d22e771f3534389d7014900ff360
David Rangel <drangelalarcon@gmail.com>
2018-12-01 18:25:14 -0800

    adding call-over to the cleverhans library

--- main.py
+++ main.py
@@ -20,6 +20,7 @@ import numpy as np
 import pathlib
 import sys
 import tensorflow as tf
+import ch_attack
 
 from tensorflow.examples.tutorials.mnist import input_data
 
@@ -27,78 +28,88 @@ from model import Model
 from partial_fgsm import PartialFgsmAttack
 from run_attack import run_attack
 
-# Parses the config.json, FLAGS, and overrides.
-parser = input_parser.Parser()
+def partial_attack(parser):
+  # Create the model and the custom attack.
+  model = Model()
 
-# Create output path directory.
-pathlib.Path(parser.save_dir).mkdir(parents=True, exist_ok=True)
+  attack = PartialFgsmAttack(model,
+                             parser.epsilon,
+                             parser.params['k'],
+                             parser.params['a'],
+                             parser.random_start,
+                             parser.loss_func)
 
-# Import the trained madry model.
-model_file = tf.train.latest_checkpoint(parser.model_dir)
-if model_file is None:
-  print('No model found')
-  sys.exit()
+  # Load MNIST data.
+  mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
 
-# Create the model and the custom attack.
-model = Model()
-attack = PartialFgsmAttack(model,
-                           parser.epsilon,
-                           parser.params['k'],
-                           parser.params['a'],
-                           parser.random_start,
-                           parser.loss_func)
+  # Restore the checkpoint (pretrained model).
+  sess = tf.Session()
+  saver = tf.train.Saver()
+  saver.restore(sess, model_file)
 
-# Load MNIST data.
-mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
+  # Generate batches for images.
+  num_eval_examples = parser.num_eval_examples
+  eval_batch_size = parser.eval_batch_size
+  num_batches = parser.num_batches
 
-# Restore the checkpoint (pretrained model).
-sess = tf.Session()
-saver = tf.train.Saver()
-saver.restore(sess, model_file)
+  x_adv = [] # adv accumulator
 
-# Generate batches for images.
-num_eval_examples = parser.num_eval_examples
-eval_batch_size = parser.eval_batch_size
-num_batches = parser.num_batches
+  print('Attacking: {}'.format(parser.adv_path))
 
-x_adv = [] # adv accumulator
+  for ibatch in range(num_batches):
+    bstart = ibatch * eval_batch_size
+    bend = min(bstart + eval_batch_size, num_eval_examples)
 
-print('Attacking: {}'.format(parser.adv_path))
+    x_batch = mnist.test.images[bstart:bend, :]
+    y_batch = mnist.test.labels[bstart:bend]
 
-for ibatch in range(num_batches):
-  bstart = ibatch * eval_batch_size
-  bend = min(bstart + eval_batch_size, num_eval_examples)
+    # parser.params has all the info needed for the attack. It reflects the
+    # tunable params in config.json.
+    x_batch_adv = attack.perturb(x_batch, y_batch, sess, parser.params)
 
-  x_batch = mnist.test.images[bstart:bend, :]
-  y_batch = mnist.test.labels[bstart:bend]
+    x_adv.append(x_batch_adv)
+  x_adv = np.concatenate(x_adv, axis=0)
 
-  # parser.params has all the info needed for the attack. It reflects the
-  # tunable params in config.json.
-  x_batch_adv = attack.perturb(x_batch, y_batch, sess, parser.params)
+  # Save the attacks if necessary.
+  if not parser.delete_attacks:
+    print('Saving: {}'.format(parser.adv_path))
+    np.save(parser.adv_path, x_adv)
 
-  x_adv.append(x_batch_adv)
-x_adv = np.concatenate(x_adv, axis=0)
+  return x_adv
 
-# Save the attacks if necessary.
-if not parser.delete_attacks:
-  print('Saving: {}'.format(parser.adv_path))
-  np.save(parser.adv_path, x_adv)
+if __name__ == '__main__':
+  # Parses the config.json, FLAGS, and overrides.
+  parser = input_parser.Parser()
 
-# Reset tensorflow. Important otherwise the eval won't run properly!
-sess.close()
-tf.reset_default_graph()
+  # Create output path directory.
+  pathlib.Path(parser.save_dir).mkdir(parents=True, exist_ok=True)
 
-# Run the evaluation on the newly generated adversarial images.
-print('Evaluating results: {}'.format(parser.y_pred_path))
-eval_results_path = None if parser.delete_attacks else parser.y_pred_path
-accuracy = run_attack(model_file,
-                      x_adv,
-                      parser.epsilon,
-                      eval_results_path)
-print('Accuracy: {}'.format(accuracy))
+  # Import the trained madry model.
+  model_file = tf.train.latest_checkpoint(parser.model_dir)
+  if model_file is None:
+    print('No model found')
+    sys.exit()
 
-# Print the reports to the output path.
-with open(parser.summary_path, 'w') as writefile:
-  summary = parser.get_summary_data()
-  summary['accuracy'] = accuracy
-  json.dump(summary, writefile, indent=4)
+  if(parser.params['partial_method'] in {'fgsm', 'bim'}):
+    x_adv = ch_attack.ch_attack(parser)
+  else:
+    x_adv = partial_attack(parser)
+  
+  # Reset tensorflow. Important otherwise the eval won't run properly!
+  tf.Session().close()
+  tf.reset_default_graph()
+
+  # Run the evaluation on the newly generated adversarial images.
+  print('Evaluating results: {}'.format(parser.y_pred_path))
+  eval_results_path = None if parser.delete_attacks else parser.y_pred_path
+  accuracy = run_attack(model_file,
+                        x_adv,
+                        parser.epsilon,
+                        eval_results_path)
+  print('Accuracy: {}'.format(accuracy))
+
+  # Print the reports to the output path.
+  with open(parser.summary_path, 'w') as writefile:
+    summary = parser.get_summary_data()
+    summary['accuracy'] = accuracy
+    json.dump(summary, writefile, indent=4)
